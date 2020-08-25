47bf83aa8d5a99bd3ea141b87ecad71e516c6224
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-08-25 13:26:10 +0100

    Boxing and stuff.

--- src/main/clojure/eutros/clojurelib/lib/class_gen.clj
+++ src/main/clojure/eutros/clojurelib/lib/class_gen.clj
@@ -43,18 +43,19 @@
               (-> (.superName node) class-from-internal)
               (into-array Class (map class-from-internal (.interfaces node)))))
 
-(defn- desc->field [desc]
-  (-> desc
-      (.replace "." "_DOT_")
+(defn- ^String munged [obj]
+  (Compiler/munge obj))
+
+(defn- extra-munged [obj]
+  (-> obj munged
       (.replace ";" "_SEMICOLON_")
-      (.replace "(" "_OPEN_PAREN_")
-      (.replace ")" "_CLOSE_PAREN_")
-      (.replace "[" "_OPEN_BRACKET_")
-      (.replace "/" "_SLASH_")))
+      (.replace "(" "_LPAREN_")
+      (.replace ")" "_RPAREN_")))
 
 (defmacro defclass
   [^Symbol top-name & forms]
-  (let [cname (str (namespace-munge *ns*) "." top-name)
+  (let [top-name (symbol (munged (str top-name)))
+        cname (str (namespace-munge *ns*) "." top-name)
         int-cname (.replace cname \. \/)
 
         obj-type (Type/getType ^Class Object)
@@ -78,128 +79,131 @@
 
     (doseq [form forms]
       (assert (keyword? (first form)) "Not a keyword!")
-      (case (first form)
-        :extends (let [super-class (eval (second form))
-                       ;; TODO generics
-                       ]
-                   (set! (.-superName node) (Type/getInternalName super-class)))
+      (let [form-next (next form)]
+        (case (first form)
+          :extends (let [super-class (eval (first form-next))
+                         ;; TODO generics
+                         ]
+                     (set! (.-superName node) (Type/getInternalName super-class)))
 
-        :implements (doseq [interface (next form)]
-                      (let [interface-class (eval interface)]
-                        (.add (.-interfaces node) (Type/getInternalName interface-class))))
+          :implements (doseq [interface form-next]
+                        (let [interface-class (eval interface)]
+                          (.add (.-interfaces node) (Type/getInternalName interface-class))))
 
-        :field (.add (.-fields node)
-                     (let [field-symbol (second form)
-                           metadata (meta field-symbol)]
-                       (FieldNode. (+ Opcodes/ACC_PUBLIC
-                                      (if (get metadata :static) Opcodes/ACC_STATIC 0)
-                                      (if (get metadata :final) Opcodes/ACC_FINAL 0))
-                                   (name field-symbol)
-                                   (Type/getDescriptor (get-type-hint field-symbol))
-                                   nil                      ;; TODO generics
-                                   (when (> (count form) 2)
-                                     (eval (last form))))))
+          :field (.add (.-fields node)
+                       (let [field-symbol (first form-next)
+                             metadata (meta field-symbol)
+                             field-name (munged (str field-symbol))]
+                         (FieldNode. (+ Opcodes/ACC_PUBLIC
+                                        (if (get metadata :static) Opcodes/ACC_STATIC 0)
+                                        (if (get metadata :final) Opcodes/ACC_FINAL 0))
+                                     field-name
+                                     (Type/getDescriptor (get-type-hint field-symbol))
+                                     nil                    ;; TODO generics
+                                     (when (> (count form) 2)
+                                       (eval (last form))))))
 
-        :method (let [method-symbol (second form)
-                      metadata (meta method-symbol)
-                      ret-class (tag-class (get (meta method-symbol) :tag 'void))
-                      params (first (nnext form))
-                      p-classes (map get-type-hint params)
-                      static (get metadata :static)
+          :method (let [method-symbol (first form-next)
+                        metadata (meta method-symbol)
+                        ret-class (tag-class (get (meta method-symbol) :tag 'void))
+                        params (second form-next)
+                        p-classes (map get-type-hint params)
+                        static (get metadata :static)
 
-                      method-node (MethodNode. (+ (cond (get metadata :private) Opcodes/ACC_PRIVATE
-                                                        (get metadata :protected) Opcodes/ACC_PRIVATE
-                                                        :else Opcodes/ACC_PUBLIC)
-                                                  (if static Opcodes/ACC_STATIC 0)
-                                                  (if (get metadata :final) Opcodes/ACC_FINAL 0))
-                                               (name method-symbol)
-                                               (Type/getMethodDescriptor (Type/getType ^Class ret-class)
-                                                                         (into-array Type (map #(-> ^Class (get-type-hint %)
-                                                                                                    (Type/getType)) params)))
-                                               nil          ;; TODO generics
-                                               (make-array String 0))
+                        method-name (munged (name method-symbol))
+                        method-node (MethodNode. (+ (cond (get metadata :private) Opcodes/ACC_PRIVATE
+                                                          (get metadata :protected) Opcodes/ACC_PRIVATE
+                                                          :else Opcodes/ACC_PUBLIC)
+                                                    (if static Opcodes/ACC_STATIC 0)
+                                                    (if (get metadata :final) Opcodes/ACC_FINAL 0))
+                                                 method-name
+                                                 (Type/getMethodDescriptor (Type/getType ^Class ret-class)
+                                                                           (into-array Type (map #(-> ^Class (get-type-hint %)
+                                                                                                      (Type/getType)) params)))
+                                                 nil        ;; TODO generics
+                                                 (make-array String 0))
 
-                      field-name (str method-prefix
-                                      (.name method-node)
-                                      (desc->field (.desc method-node)))
+                        field-name (str method-prefix
+                                        (.name method-node)
+                                        (extra-munged (.desc method-node)))
 
-                      ga (GeneratorAdapter. method-node
-                                            (.-access method-node)
-                                            (.-name method-node)
-                                            (.-desc method-node))
-                      instructions (.-instructions method-node)]
+                        ga (GeneratorAdapter. method-node
+                                              (.-access method-node)
+                                              (.-name method-node)
+                                              (.-desc method-node))
+                        instructions (.-instructions method-node)]
 
-                  (doseq [param params]
-                    (.visitParameter method-node (name param) 0))
+                    (doseq [param params]
+                      (.visitParameter method-node (name param) 0))
 
-                  (.add (.-fields node)
-                        (FieldNode. (+ Opcodes/ACC_PUBLIC
-                                       Opcodes/ACC_STATIC)
-                                    field-name
-                                    ifn-desc
-                                    nil
-                                    nil))
+                    (.add (.-fields node)
+                          (FieldNode. (+ Opcodes/ACC_PUBLIC
+                                         Opcodes/ACC_STATIC)
+                                      field-name
+                                      ifn-desc
+                                      nil
+                                      nil))
 
-                  (.add instructions
-                        (FieldInsnNode. Opcodes/GETSTATIC
-                                        int-cname
-                                        field-name
-                                        ifn-desc))
-                  (when-not static
                     (.add instructions
-                          (VarInsnNode. Opcodes/ALOAD 0)))
+                          (FieldInsnNode. Opcodes/GETSTATIC
+                                          int-cname
+                                          field-name
+                                          ifn-desc))
+                    (when-not static
+                      (.add instructions
+                            (VarInsnNode. Opcodes/ALOAD 0)))
 
-                  ;; breaks with more than 19 arguments...
-                  (dotimes [i (count p-classes)]
-                    (.loadArg ga i)
-                    ;; (Compiler$HostExpr/emitBoxReturn nil ga (nth p-classes i)) TODO boxing
-                    )
+                    ;; breaks with more than 19 arguments...
+                    (dotimes [i (count p-classes)]
+                      (let [param-type (Type/getType ^Class (nth p-classes i))]
+                        (.loadArg ga i)
+                        (.valueOf ga param-type)))
 
-                  (.add instructions
-                        (MethodInsnNode. Opcodes/INVOKEINTERFACE
-                                         (Type/getInternalName IFn)
-                                         "invoke"
-                                         (Type/getMethodDescriptor obj-type
-                                                                   (into-array Type
-                                                                               (repeat ((if static
-                                                                                          identity
-                                                                                          inc)
-                                                                                        (count p-classes))
-                                                                                       obj-type)))))
+                    (.add instructions
+                          (MethodInsnNode. Opcodes/INVOKEINTERFACE
+                                           (Type/getInternalName IFn)
+                                           "invoke"
+                                           (Type/getMethodDescriptor obj-type
+                                                                     (into-array Type
+                                                                                 (repeat ((if static
+                                                                                            identity
+                                                                                            inc)
+                                                                                          (count p-classes))
+                                                                                         obj-type)))))
 
-                  (when (= ret-class Void/TYPE)
-                    (.add instructions (InsnNode. Opcodes/POP)))
+                    (when (= ret-class Void/TYPE)
+                      (.add instructions (InsnNode. Opcodes/POP)))
 
-                  (doto ga
-                    (.unbox (Type/getType ^Class ret-class))
-                    (.returnValue))
+                    (doto ga
+                      (.unbox (Type/getType ^Class ret-class))
+                      (.returnValue))
 
-                  (.add (.-methods node)
-                        method-node))
+                    (.add (.-methods node)
+                          method-node))
 
-        :constructor (let [params (second form)
-                           p-types (map (fn [param]
-                                          (-> ^Class (get-type-hint param)
-                                              (Type/getType)))
-                                        params)
-                           method-desc (Type/getMethodDescriptor Type/VOID_TYPE
-                                                                 (into-array Type p-types))
-                           method-node (MethodNode. Opcodes/ACC_PUBLIC
-                                                    "<init>"
-                                                    method-desc
-                                                    nil (make-array String 0))]
-                       (doto (.-instructions method-node)
-                         (.add (VarInsnNode. Opcodes/ALOAD 0))
-                         (.add (MethodInsnNode. Opcodes/INVOKESPECIAL
-                                                (.-superName node)
-                                                "<init>"
-                                                method-desc))
-                         (.add (InsnNode. Opcodes/RETURN)))
+          :constructor (let [params (second form)
+                             p-types (map (fn [param]
+                                            (-> ^Class (get-type-hint param)
+                                                (Type/getType)))
+                                          params)
+                             method-desc (Type/getMethodDescriptor Type/VOID_TYPE
+                                                                   (into-array Type p-types))
+                             method-node (MethodNode. Opcodes/ACC_PUBLIC
+                                                      "<init>"
+                                                      method-desc
+                                                      nil (make-array String 0))]
+                         (doto (.-instructions method-node)
+                           (.add (VarInsnNode. Opcodes/ALOAD 0))
+                           (.add (MethodInsnNode. Opcodes/INVOKESPECIAL
+                                                  (.-superName node)
+                                                  "<init>"
+                                                  method-desc))
+                           (.add (InsnNode. Opcodes/RETURN)))
 
-                       (.add (.-methods node)
-                             method-node))
+                         (.add (.-methods node)
+                               method-node))
 
-        ))
+          )))
 
     (load-tree node)
 
@@ -207,8 +211,9 @@
        (import ~(symbol cname))
 
        ~@(->> (map #(case (first %)
-                      :method (let [params (first (nnext %))
-                                    ret-class (tag-class (get (meta (second %))
+                      :method (let [[name-symbol params & body] (next %)
+                                    fn-meta (meta name-symbol)
+                                    ret-class (tag-class (get fn-meta
                                                               :tag 'void))
                                     p-types (map (fn [param]
                                                    (-> ^Class (get-type-hint param)
@@ -216,21 +221,20 @@
                                                  params)]
                                 `(set! (. ~top-name
                                           ~(symbol (str method-prefix
-                                                        (name (second %))
-                                                        (desc->field
+                                                        (name name-symbol)
+                                                        (extra-munged
                                                           (Type/getMethodDescriptor
                                                             (Type/getType ^Class ret-class)
                                                             (into-array Type p-types))))))
                                        ~(cons 'fn
-                                              (cons
-                                                (let [p-seq (map fn-hint-safe params)]
-                                                  (if (get (meta (second %))
-                                                           :static)
-                                                    (apply vector p-seq)
-                                                    (apply vector
-                                                           (with-meta 'this {:tag top-name})
-                                                           p-seq)))
-                                                (next (nnext %))))))
+                                              (cons (let [p-seq (map fn-hint-safe params)]
+                                                      (if (get fn-meta
+                                                               :static)
+                                                        (apply vector p-seq)
+                                                        (apply vector
+                                                               (with-meta 'this {:tag top-name})
+                                                               p-seq)))
+                                                    body))))
                       nil)
                    forms)
               (filter identity))
